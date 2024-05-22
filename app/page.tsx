1ac6465752b46f10d45c764881f2fe1f2e721adc
Emma Zheng <73250770+skkra0@users.noreply.github.com>
2024-05-22 16:23:15 +0000

    alignment and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,27 @@
 import Image from "next/image";
 import logo from "/public/logo.png";
+import loading from "/public/loading.gif";
 export default function Home() {
   return (
     <>
-    <div className="main">
-      <div className="border-slate-300 border-2 rounded-md w-60 h-12 flex gap-2 justify-between">
-      <label htmlFor="captcha">
-        <input 
-      className="appearance-none w-8 h-8 border-slate-400 border-2 rounded-sm mr-2" id="captcha" type="checkbox"/>
-      Are you a robot?
-      </label>
-      
+    <div className="main h-screen flex flex-row justify-center items-center">
+      <div className="border-slate-300 bg-slate-50  text-slate-700 border-2 rounded-sm w-72 pt-1 flex gap-1 items-center">
+      <input 
+      className="appearance-none w-8 h-8 border-slate-400 border-2 rounded-sm mr-1 ml-3 mb-2 mt-2" id="captcha" type="checkbox"/>
+      <label htmlFor="captcha" className="mr-1 ml-3 mb-2 mt-2">Are you a robot?</label>
+      <Image
+      width={40}
+      height={40}
+      src={loading}
+      alt="loading..."
+      />
       <Image 
-      width={20}
-      height={20}
+      width={60}
+      height={60}
       src={logo} 
-      alt="ReCaptcha logo"/>
+      alt="ReCaptcha logo"
+      className="ml-auto"
+      />
       </div>
     </div>
     </>
